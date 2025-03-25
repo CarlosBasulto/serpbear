14a119098b7064a8911da5334e0e7937dcf13597
Carlos Basulto Pardo <136237882+CarlosBasulto@users.noreply.github.com>
2025-03-25 17:30:48 +0100

    Update serper.ts
    add city to scrapper

--- scrapers/services/serper.ts
+++ scrapers/services/serper.ts
@@ -4,7 +4,46 @@ interface SerperResult {
    position: number,
 }
 
-const serper:ScraperSettings = {
+const serper: ScraperSettings = {
+   id: 'serper',
+   name: 'Serper.dev',
+   website: 'serper.dev',
+   allowsCity: true,
+   scrapeURL: (keyword, settings, countryData) => {
+      const country = keyword.country || 'US';
+      const lang = countryData[country][2];
+
+      const locationString = (keyword.city)
+         ? ` ${keyword.city}, ${countryData[country][0]}`
+         : '';
+
+      const fullQuery = `${keyword.keyword}${locationString}`;
+
+      const url = `https://google.serper.dev/search?q=${encodeURIComponent(fullQuery)}&gl=${country}&hl=${lang}&num=100&apiKey=${settings.scaping_api}`;
+      
+      console.log('Serper URL :', url);
+      return url;
+   },
+   resultObjectKey: 'organic',
+   serpExtractor: (content) => {
+      const extractedResult = [];
+      const results: SerperResult[] = (typeof content === 'string') ? JSON.parse(content) : content as SerperResult[];
+
+      for (const { link, title, position } of results) {
+         if (title && link) {
+            extractedResult.push({
+               title,
+               url: link,
+               position,
+            });
+         }
+      }
+      return extractedResult;
+   },
+};
+
+export default serper;
+/*const serper:ScraperSettings = {
    id: 'serper',
    name: 'Serper.dev',
    website: 'serper.dev',
@@ -33,4 +72,4 @@ const serper:ScraperSettings = {
    },
 };
 
-export default serper;
+export default serper;*/
